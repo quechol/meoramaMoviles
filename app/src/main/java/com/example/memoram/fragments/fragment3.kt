908cdc9fe0d99bdc8902286be651b6ebdc5b0c8f
quechol <zaratelogan@gmail.com>
2021-09-26 22:49:42 -0500

    variables de memorama

--- app/src/main/java/com/example/memoram/fragments/fragment3.kt
+++ app/src/main/java/com/example/memoram/fragments/fragment3.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import com.example.memoram.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,11 +19,36 @@ private const val ARG_PARAM2 = "param2"
  * Use the [fragment3.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
+
 class fragment3 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    var puntuacion: Int = 0
+    var aciertos: Int = 0
+
+    lateinit var img0: ImageButton
+    lateinit var img1: ImageButton
+    lateinit var img2: ImageButton
+    lateinit var img3: ImageButton
+    lateinit var img4: ImageButton
+    lateinit var img5: ImageButton
+    lateinit var img6: ImageButton
+    lateinit var img7: ImageButton
+    lateinit var img8: ImageButton
+    lateinit var img9: ImageButton
+    lateinit var img10: ImageButton
+    lateinit var img11: ImageButton
+    lateinit var img12: ImageButton
+    lateinit var img13: ImageButton
+    lateinit var img14: ImageButton
+    lateinit var img15: ImageButton
+
+    //ImageButton[] tablero = new ImageButton[16];
+    val buttons = arrayOf(img0,img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -35,7 +62,12 @@ class fragment3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment3, container, false)
+        val vista = inflater.inflate(R.layout.fragment_fragment3, container, false)
+
+        //img1 = vista.findViewById<ImageButton>(R.id.img0)
+        //print(buttons[2])
+
+        return vista
     }
 
     companion object {
